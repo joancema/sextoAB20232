3cee8b845991501743a0c5f0596b7bc509f2a42f
John Cevallos <joancemac@gmail.com>
2023-08-29 21:09:47 -0500

    safadf inicial

---
+++ 01/app.ts
@@ -0,0 +1,17 @@
+// export const nombre= 'Joancema';
+// console.log(nombre)
+
+
+let nombre:string = '0'
+
+// nombre=345
+class persona{
+    constructor(nombre:string){
+        this.nombre=nombre;
+    }
+    nombre:string;
+
+}
+let john= new persona('John');
+console.log(john.nombre) 
+
---
+++ 01/dist/app.js
@@ -0,0 +1,21 @@
+"use strict";
+// export const nombre= 'Joancema';
+// console.log(nombre)
+let nombre = '0';
+// nombre=345
+class persona {
+    constructor(nombre) {
+        this.nombre = nombre;
+    }
+}
+let john = new persona('John');
+console.log(john.nombre);
+
+
+
+
+
+//# sourceMappingURL=app.js.map
+
+
+
