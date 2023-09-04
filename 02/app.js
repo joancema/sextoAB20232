39af6f439d7f28ec3ba932d36c0859e6a429f054
John Cevallos <joancemac@gmail.com>
2023-09-03 21:55:57 -0500

    adf

--- 02/app.js
+++ 02/app.js
@@ -1,1 +1,14 @@
-console.log('Hello World');
+const ejemplo ={ id: 1, nombre: 'Ejemplo' };
+
+const clone = {...ejemplo};
+
+clone.nombre='Ejemplo 2';
+
+// console.log(ejemplo);
+// console.log(clone);
+
+const { nombre, ...rest } =  ejemplo;
+
+console.log(nombre);
+console.log(rest);
+
